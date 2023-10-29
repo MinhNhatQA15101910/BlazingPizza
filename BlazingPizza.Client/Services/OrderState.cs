a4643cee685dc017d218dd48944224e5bd65a1b6
Do Nhat <dmnhat.pt@gmail.com>
2023-10-29 15:24:21 +0700

    Refactor state management

---
+++ BlazingPizza.Client/Services/OrderState.cs
@@ -0,0 +1,45 @@
+namespace BlazingPizza.Client.Services;
+
+public class OrderState 
+{
+    public bool ShowingConfigureDialog { get; private set; }
+    public Pizza ConfiguringPizza { get; private set; }
+    public Order Order { get; private set; } = new Order();
+
+    public void ShowConfigurePizzaDialog(PizzaSpecial special)
+    {
+        ConfiguringPizza = new()
+        {
+            Special = special,
+            SpecialId = special.Id,
+            Size = Pizza.DefaultSize,
+            Toppings = new()
+        };
+
+        ShowingConfigureDialog = true;
+    }
+
+    public void ConfirmConfigurePizzaDialog()
+    {
+        Order.Pizzas.Add(ConfiguringPizza);
+        ConfiguringPizza = null;
+
+        ShowingConfigureDialog = false;
+    }
+
+    public void CancelConfigurePizzaDialog()
+    {
+        ConfiguringPizza = null;
+        ShowingConfigureDialog = false;
+    }
+
+    public void RemoveConfiguredPizza(Pizza pizza)
+    {
+        Order.Pizzas.Remove(pizza);
+    }
+
+    public void ResetOrder()
+    {
+        Order = new Order();
+    }
+}
