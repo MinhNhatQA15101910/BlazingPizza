58d4ac0eedba4d194675de43b1f761d8905c63dd
Do Nhat <dmnhat.pt@gmail.com>
2023-10-30 18:58:18 +0700

    Preserving order state across the redirection flow

--- BlazingPizza.Client/Program.cs
+++ BlazingPizza.Client/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddScoped<OrderState>();
 
 // Add auth services
-builder.Services.AddApiAuthorization();
+builder.Services.AddApiAuthorization<PizzaAuthenticationState>();
 
 builder.Services.AddHttpClient<OrdersClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
--- BlazingPizza.Client/Services/OrderState.cs
+++ BlazingPizza.Client/Services/OrderState.cs
@@ -42,4 +42,9 @@ public class OrderState
     {
         Order = new Order();
     }
+
+    public void ReplaceOrder(Order order)
+    {
+        Order = order;
+    }
 }
