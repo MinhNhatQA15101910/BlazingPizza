9a969393830f07af4dad0de2d445997a4b076ec8
Do Nhat <dmnhat.pt@gmail.com>
2023-10-30 19:01:34 +0700

    Customize the logout experience

--- BlazingPizza.Client/Program.cs
+++ BlazingPizza.Client/Program.cs
@@ -12,7 +12,10 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddScoped<OrderState>();
 
 // Add auth services
-builder.Services.AddApiAuthorization<PizzaAuthenticationState>();
+builder.Services.AddApiAuthorization<PizzaAuthenticationState>(options => 
+{
+    options.AuthenticationPaths.LogOutSucceededPath = "";
+});
 
 builder.Services.AddHttpClient<OrdersClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
